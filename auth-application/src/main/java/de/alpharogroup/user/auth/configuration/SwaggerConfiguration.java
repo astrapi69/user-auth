e802c0ffbe2c6537e854ae929bd8cb808b12b4ce
astrapi69 <asterios.raptis@gmx.net>
2020-07-30 17:46:46 +0200

    new abstract class AbstractSwaggerConfiguration created

--- auth-application/src/main/java/de/alpharogroup/user/auth/configuration/SwaggerConfiguration.java
+++ auth-application/src/main/java/de/alpharogroup/user/auth/configuration/SwaggerConfiguration.java
@@ -1,42 +1,58 @@
 package de.alpharogroup.user.auth.configuration;
 
-import static springfox.documentation.builders.PathSelectors.regex;
-
-import java.util.Collections;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@Configuration
-@EnableSwagger2
-public class SwaggerConfiguration
+@Configuration @EnableSwagger2 public class SwaggerConfiguration
+	extends AbstractSwaggerConfiguration
 {
-	private static final String VERSION_API_1 = "/v1";
 
-	@Bean
-	public Docket api()
+	@Bean public Docket api()
 	{
-		return new Docket(DocumentationType.SWAGGER_2)
-			.select()
-			.apis(RequestHandlerSelectors.basePackage("de.alpharogroup.user.auth"))
-			.paths(regex(VERSION_API_1 + "/.*|"))
-			.build()
-			.apiInfo(metaData());
+		return super.api();
 	}
 
-	private ApiInfo metaData()
+	protected ApiInfo metaData()
 	{
-		return new ApiInfo("User Auth REST API",
-			"REST API for user authorisation and authentication", "v1", "",
-			new Contact("user-auth org.", "www.user-auth.org", ""), "", "",
-			Collections.emptyList());
+		return super.metaData();
+	}
+
+	@Override public String getBasePackage()
+	{
+		return "de.alpharogroup.user.auth";
+	}
+
+	@Override public String getApiInfoTitle()
+	{
+		return "User Auth REST API";
+	}
+
+	@Override public String getApiInfoDescription()
+	{
+		return "REST API for user authorisation and authentication";
+	}
+
+	@Override public String getApiInfoVersion()
+	{
+		return ApplicationConfiguration.VERSION_API_1;
+	}
+
+	@Override public String getContactName()
+	{
+		return "user-auth org.";
+	}
+
+	@Override public String getContactUrl()
+	{
+		return "www.user-auth.org";
+	}
+
+	@Override public String getDocketPathsPathRegex()
+	{
+		return ApplicationConfiguration.REST_VERSION + "/.*|";
 	}
 
 }
